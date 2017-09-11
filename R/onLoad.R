058da5e1b50fadf89acebd0220ffdb18a472bd51
Robrecht Cannoodt <brechtus@gmail.com>
2017-09-11 17:30:18 +0200

    added glue glue imports

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,3 +1,4 @@
+#' @importFrom glue glue
 .onLoad <- function(libname, pkgname) {
   if(!dir.exists(glue::glue("bash {find.package('Wishbone')}/venv"))) {
     reinstall()
@@ -5,6 +6,7 @@
 }
 
 #' @export
+#' @importFrom glue glue
 reinstall <- function() {
   system(glue::glue("bash {find.package('Wishbone')}/make {find.package('Wishbone')}/venv"))
 }
